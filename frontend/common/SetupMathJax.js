0571df7cf9560d09d012e1f8dd7f2329db21f660
mcs <marcus@psydat.com>
2022-02-28 20:30:52 -0600

    activated eqn numbering
    add `tags: 'ams'` to the `tex` config block

--- frontend/common/SetupMathJax.js
+++ frontend/common/SetupMathJax.js
@@ -48,6 +48,7 @@ export const setup_mathjax = () => {
             },
         },
         tex: {
+            tags: 'ams',
             inlineMath: [
                 ["$", "$"],
                 ["\\(", "\\)"],
